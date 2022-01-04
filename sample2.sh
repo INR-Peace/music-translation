654da729fec57df4ebb9e654b46461466d69f089
Yejin Lim <48744667+dla4777@users.noreply.github.com>
2022-01-04 19:54:57 +0900

    Create sample2.sh

---
+++ sample2.sh
@@ -0,0 +1,14 @@
+#copy orchestrator
+
+DATE=`date +%d_%m_%Y`
+CODE=src
+OUTPUT=results/${DATE}/$1
+checkpoint_path=$1
+shift
+
+echo "Sampling"
+python ${CODE}/data_samples.py --data musicnet/preprocessed/Bach_Solo_Piano/ --output ${OUTPUT} -n 4 --seq 80000 # Cello
+
+echo "Generating"
+python ${CODE}/run_on_files.py --files ${OUTPUT} --batch-size 4 --checkpoint $checkpoint_path --output-next-to-orig --decoders $@ --py
+
